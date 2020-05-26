b9bcb4451985d4694bbe92dd8a6c4c237fd0ce20
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-05-26 21:45:42 +0100

    first commit

---
+++ Crud-project/Controllers/ReceiptController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Crud_project.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Crud_project.Controllers
+{
+    public class ReceiptController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ReceiptController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var displaydata = _db.ReceiptTable.ToList();
+            return View(displaydata);
+        }
+    }
+}
---
+++ Crud-project/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_project.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<NewReceiptClass> ReceiptTable { get; set; }
+    }
+}
